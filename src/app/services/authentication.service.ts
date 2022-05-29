2e052182218ba909663f97aa26fbc6ed418fb42e
carlos-ERL <caedrobleslo@ittepic.edu.mx>
2022-05-29 16:13:41 -0600

    Agregado de logo en Assets y cambios en intro

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Storage } from '@capacitor/storage';
+import { BehaviorSubject } from 'rxjs';
+const TOKEN_KEY='my-token';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
-
-  constructor() { }
+  isAuthenticated: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(null)
+  token = '';
+  constructor(private http: HttpClient) { 
+    this.loadToken();
+  }
+  async loadToken(){}
+  login(){}
+  logout(){}
 }
